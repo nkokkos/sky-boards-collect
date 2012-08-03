3e7b49e80db2beb34370a60044e92126481715fd
eldial@gmail.com <eldial@gmail.com@bc651712-035d-dc3b-e712-0c06c767e616>
2012-08-03 14:16:58 +0000

    Reorganize

---
+++ trunk/sky-boards-collect/src/se/sics/contiki/collect/NodeAR1000.java
@@ -0,0 +1,87 @@
+/*
+ * NodeAR1000
+ *
+ * Author  : Eloy Díaz <eldial@gmail.com>
+ * Created : 15 jun 2012
+ */
+
+/**
+ * 
+ */
+package se.sics.contiki.collect;
+
+public class NodeAR1000 extends Node implements SensorInfo{
+	
+	public int type=AR1000;
+
+	public NodeAR1000(String nodeID) {
+		super(nodeID);
+		addNodeSensors();	
+	}
+
+	public NodeAR1000(String nodeID, String nodeName) {
+		super(nodeID, nodeName);
+	}
+	
+	public void addNodeSensors(){
+		sensors.put(CO, new NodeSensor("CO", 
+				CO,this.getID()));
+		sensors.put(CO2, new NodeSensor("CO2", 
+				CO2,this.getID()));
+		sensors.put(DUST, new NodeSensor("Dust", 
+				DUST,this.getID()));
+		setDefaultValues();
+	}
+	
+	public void setDefaultValues(){
+		setCOValues();
+		setCO2Values();
+		setDustValues();
+	}
+	
+	public void setDefaultValues(String sensorName)
+	{
+		NodeSensor sensor=sensors.get(this.keyConv(sensorName));
+		if (sensor==null) return;
+		if (sensorName.equals("CO")){
+			setCOValues();
+			return;
+		}	
+		if (sensorName.equals("CO2")){
+			setCO2Values();
+			return;
+		}
+		if (sensorName.equals("Dust")){
+			setDustValues();
+		}	
+	}
+	
+	private void setCOValues()
+	{
+		NodeSensor s=sensors.get(CO);
+		s.setVar("Vref", 2.5);
+		s.setVar("RL", 20000.0);
+		s.setVar("R0", 280000.0);
+		s.setVar("Vcc", 3.0);
+		s.setVar("case2_v1", 300);
+		s.setVar("case2_v2", 250);
+		s.setVar("case3_v1", 3600);
+		s.setVar("case3_v2", 1900);
+	}
+
+	private void setCO2Values()
+	{
+		NodeSensor s=sensors.get(CO2);
+		s.setVar("Vref", 2.5);
+		s.setVar("v1", 1000.0);
+		s.setVar("v2", 200.0);
+	}
+	
+	private void setDustValues()
+	{
+		NodeSensor s=sensors.get(DUST);
+		s.setVar("Vref", 2.5);
+		s.setVar("v1", 0.228);
+		s.setVar("v2", 0.03);	
+	}
+}
---
+++ trunk/sky-boards-collect/src/se/sics/contiki/collect/PublisherSense.java
@@ -0,0 +1,141 @@
+/*
+ * CosmPublisher
+ *
+ * Author  : Eloy Díaz <eldial@gmail.com>
+ * Created : 30 jul 2012
+ */
+
+package se.sics.contiki.collect;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Hashtable;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+
+public class PublisherSense extends Thread{
+	
+	final static String SenseServer = "http://api.sen.se/events/";
+	
+	private String APIkey;
+	Hashtable<String,String> feedTable;
+
+	public PublisherSense(Hashtable<String,String> feedTable, String key){
+		APIkey=key;
+		this.feedTable=feedTable;
+	}
+
+
+	public String getAPIkey() {
+		return APIkey;
+	}
+
+	public void setAPIkey(String APIkey) {
+		this.APIkey = APIkey;
+	}
+	
+	public String constructMsgOpenSense(Hashtable<String,String> feedTable){
+		StringBuffer msg = new StringBuffer();
+		char separator=',';
+		for(Object key: feedTable.keySet()) {
+			msg.append("{\"feed_id\" : "+key+", " +
+					"	  \"value\" : \""+feedTable.get(key)+"\"}"+separator);
+		}
+		// remove last separator
+		String msgbody=msg.toString();
+		msgbody=msgbody.substring(0, msgbody.length()-1);
+		return "["+msgbody+"]";
+	}
+	
+	protected void logLine(String line, boolean stderr, Throwable e) {
+		if (stderr) {
+			System.err.println("sen.se Publisher@ " + line);
+		} else {
+			System.out.println("sen.se Publisher@ " + line);
+		}
+		if (e != null) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		if (feedTable.isEmpty()) return; //nothing to feed
+    
+        URL url = null;
+        try {
+            url = new URL(SenseServer);
+        } catch (MalformedURLException ex) {
+        	logLine("Bad URL", false, ex);
+        	return;
+        }
+
+        HttpURLConnection urlConn = null;
+        try {
+            // URL connection channel.
+            urlConn = (HttpURLConnection) url.openConnection();
+        } catch (IOException ex) {
+        	logLine("Unable to open connection", false, ex);
+        	return;
+        }
+
+        urlConn.setDoInput (true);
+        urlConn.setDoOutput (true);
+        urlConn.setUseCaches (false);
+
+        try {
+            urlConn.setRequestMethod("POST");
+        } catch (ProtocolException ex) {
+        	logLine("Unable to connect", false, ex);
+        	return;
+        }
+        
+        urlConn.setRequestProperty("Content-Type","application/json");
+        urlConn.setRequestProperty("sense_key", APIkey);
+
+        try {
+            urlConn.connect();
+        } catch (IOException ex) {
+        	logLine("Unable to connect", false, ex);
+        	return;
+        }
+
+        DataOutputStream output = null;
+        BufferedReader input = null;
+
+        try {
+            output = new DataOutputStream(urlConn.getOutputStream());
+        } catch (IOException ex) {
+        	logLine("Unable to get output stream", false, ex);
+        	return;
+        }
+        
+        String content=constructMsgOpenSense(feedTable);
+
+        try {
+            output.writeBytes(content);
+            output.flush();
+            output.close();
+        } catch (IOException ex) {
+        	logLine("Problem with output stream", false, ex);
+        	return;
+        }
+
+        String str = null;
+        try {
+        	System.out.println("sen.se Publisher@ "+urlConn.getResponseCode()+" "+
+        			urlConn.getResponseMessage());
+            input= new BufferedReader(new InputStreamReader
+            		(urlConn.getInputStream()));
+            while (null != ((str = input.readLine()))) {
+                System.out.println(str);
+            }
+            input.close ();
+        } catch (IOException ex) {
+        	logLine("Something went wrong", false, ex);
+        }
+	}
+}
+
